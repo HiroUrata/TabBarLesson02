fb44209e0afd1888c39c274e3fa7191f6b6f6d13
HiroUrata <1712026@i.softbank.jp>
2021-08-24 17:51:47 +0900

    tableView 表示内容　追加

--- TabBarLesson02/ViewController.swift
+++ TabBarLesson02/ViewController.swift
@@ -14,9 +14,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
       
+        tableView.delegate = self
+        tableView.dataSource = self
         
     }
 
 
 }
 
+extension ViewController:UITableViewDelegate,UITableViewDataSource{
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 41
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = String(indexPath.row)
+        
+        return cell
+    }
+    
+    
+    
+    
+}
