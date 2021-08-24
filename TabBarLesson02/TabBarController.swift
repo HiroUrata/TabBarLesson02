e9fd997de5c3c8a17a35dc2c96be26fa79c7e595
HiroUrata <1712026@i.softbank.jp>
2021-08-24 17:18:00 +0900

    TabBar 見た目を調整

--- TabBarLesson02/TabBarController.swift
+++ TabBarLesson02/TabBarController.swift
@@ -12,8 +12,18 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tabBar.barTintColor = .black
+        tabBar.tintColor = .white
+        
+        tabBar.layer.cornerRadius = 45.0
+        tabBar.layer.masksToBounds = true
+        
+        tabBar.layer.borderWidth = 3.0
+        tabBar.layer.borderColor = UIColor.white.cgColor
+        
         
     }
+    
     
 
 }
